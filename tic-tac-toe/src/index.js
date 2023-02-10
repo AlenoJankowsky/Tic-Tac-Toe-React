59276d66edf5136315871b1f4fcea941043ae03c
Aleno <aleno.jankowsky@5minds.de>
2023-02-10 14:31:13 +0100

    Remove whitespace

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -130,7 +130,7 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
-  
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
